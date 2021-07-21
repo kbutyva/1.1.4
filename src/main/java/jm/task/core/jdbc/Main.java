fc0bc0f01909145f9ffd45fc15d25b52b04faedf
kbutyva <84053102+kbutyva@users.noreply.github.com>
2021-07-21 23:05:50 +0800

    Initial commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,14 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-  //     userDaoJDBC.createUsersTable();
- //     userDaoJDBC.dropUsersTable();
-       userDaoJDBC.saveUser("Aylana", "Kuular", (byte) 2);
-//       userDaoJDBC.removeUserById(100);
-//      userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.getAllUsers();
-        System.out.println(userDaoJDBC.getAllUsers());
-
+//        userDaoJDBC.createUsersTable();
+//        userDaoJDBC.dropUsersTable();
+//        userDaoJDBC.saveUser("Aylana", "Kuular", (byte) 2);
+//        userDaoJDBC.removeUserById(100);
+        userDaoJDBC.cleanUsersTable();
+//        userDaoJDBC.getAllUsers();
+//        System.out.println(userDaoJDBC.getAllUsers());
 
 
     }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
